54c19686a9d648f8e20f34a3a975f224ea700313
AnthonyBoutherin <anthony.boutherin@gmail.com>
2024-02-26 15:34:21 +0100

    text and Button center

--- app/src/main/java/com/example/mydiceroller/MainActivity.kt
+++ app/src/main/java/com/example/mydiceroller/MainActivity.kt
@@ -15,32 +15,33 @@ import com.example.mydiceroller.ui.theme.MyDiceRollerTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MyDiceRollerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+//        setContent {
+//            MyDiceRollerTheme {
+//                // A surface container using the 'background' color from the theme
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colorScheme.background
+//                ) {
+//                    Greeting("Anthony")
+//                }
+//            }
+//        }
+        setContentView(R.layout.activity_main)
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//        text = "Hello $name! Let's learn Kotlin ! :)",
+//        modifier = modifier
+//    )
+//}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyDiceRollerTheme {
-        Greeting("Android")
-    }
-}
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    MyDiceRollerTheme {
+//        Greeting("Android")
+//    }
+//}
