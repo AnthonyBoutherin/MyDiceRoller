843798c17eef15ecf9b0709f20e6c2d92c6d5250
AnthonyBoutherin <anthony.boutherin@gmail.com>
2024-02-26 16:28:36 +0100

    clean code

--- app/src/main/java/com/example/mydiceroller/MainActivity.kt
+++ app/src/main/java/com/example/mydiceroller/MainActivity.kt
@@ -3,48 +3,26 @@ package com.example.mydiceroller
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.mydiceroller.ui.theme.MyDiceRollerTheme
 import java.util.Random
-
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContent {
-//            MyDiceRollerTheme {
-//                // A surface container using the 'background' color from the theme
-//                Surface(
-//                    modifier = Modifier.fillMaxSize(),
-//                    color = MaterialTheme.colorScheme.background
-//                ) {
-//                    Greeting("Anthony")
-//                }
-//            }
-//        }
+
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
-//        rollButton.text = "Let's Roll"
+
         rollButton.setOnClickListener {
-//            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
             rollDice()
         }
     }
 
     private fun rollDice() {
-//        val resultText: TextView = findViewById(R.id.result_text)
-
+        // Getting random Int  random() -> ON TOP FILE ->[import java.util.Random]
         val randomInt = Random().nextInt(6) + 1
+
+        // Case 1... Case 2... etc.
         val drawableResource = when (randomInt) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
@@ -53,27 +31,7 @@ class MainActivity : ComponentActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-
         val diceImage: ImageView = findViewById(R.id.dice_image)
         diceImage.setImageResource(drawableResource)
-
-//        resultText.text = randomInt.toString()
     }
-
 }
-
-//@Composable
-//fun Greeting(name: String, modifier: Modifier = Modifier) {
-//    Text(
-//        text = "Hello $name! Let's learn Kotlin ! :)",
-//        modifier = modifier
-//    )
-//}
-
-//@Preview(showBackground = true)
-//@Composable
-//fun GreetingPreview() {
-//    MyDiceRollerTheme {
-//        Greeting("Android")
-//    }
-//}
